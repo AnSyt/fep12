fbe199fa330c932052982f031bf56421c46bdcd1
Julia Sessa <chanievs@gmail.com>
2018-10-29 19:59:21 +0200

    initial commit

---
+++ lesson2/index.js
@@ -0,0 +1,123 @@
+'use strict';
+//2.1 - 2.8
+/*Чему будет равен x в примере ниже?
+
+ var a = 2;
+
+ var x = 1 + (a *= 2);*/
+
+// Result = 5;
+
+//https://learn.javascript.ru/operators
+
+/*
+ 1
+Comment
+ 2
+*/
+
+// Comment
+
+//alert('index.js');
+
+//var a;
+//a = 2;
+
+// var d = 5;
+// var e = 6;
+// var f = 7;
+//
+//
+//
+// var a = 2,
+//     b = 3,
+//     c = 4;
+//
+// alert(a)
+//
+// ;
+
+var a = "1";
+var a2 = 2;
+
+//alert(a2); //2
+
+a2 = a;
+
+//alert(a2); //1
+
+var b = 2;
+var c = true;
+var d = false;
+var e = null;
+var f = undefined;
+
+var g;
+
+var h = {};
+
+//alert(g);
+
+typeof e;
+
+// Operators
+var x = 1;
+
+x = -x;
+console.log( x );
+
+var x1 = "1", x2 = 2;
+
+
+console.log( typeof(-x1 + x2) );
+console.log( -x1 + x2, ' <- result' );
+
+console.log((2 + 2) * 2);
+
+//a = b = c = 2 + 2;
+
+console.log( 5 % 2 ); // 1, остаток от деления 5 на 2
+console.log( 8 % 3 ); // 2, остаток от деления 8 на 3
+console.log( 6 % 3 ); // 0, остаток от деления 6 на 3
+
+console.log(3 % 2);
+console.log(8 % 2);
+console.log(6 % 2);
+
+var x3 = 1;
+//x3 = x3 + x3;
+//x3 += x3;
+//x3 += 4; // x3 = x3 +4
+console.log(x3, '<-x3');
+
+x3 = x3 + 1;
+console.log(x3, 'x3 = x3 + 1'); // 2
+x3 += 1;
+console.log(x3, 'x3 += 1'); // 3
+x3++;
+console.log(x3, 'x3++'); // 4
+x3++;
+x3++;
+x3++;
+console.log(x3--);
+console.log(x3);
+
+
+
+
+
+
+var y = 1;
+console.log(++y, '<- y1'); //2
+console.log(y++, '<- y2'); //1
+console.log(y, '<- y3'); //3
+
+var a5 = (5, 6);
+
+alert( a5 );
+
+
+
+
+
+
---
+++ lesson3/index.js
@@ -0,0 +1,139 @@
+'use strict';
+https://wtfjs.com/
+
+//2.9 - 2.15
+var a = "1";
+var b = "2";
+
+console.log(+a + +b);
+
+// > < == >= <= != === !==
+
+console.log(1 > 2);
+console.log('bbc' > 'acd');
+
+console.log(1 !== '1');
+
+console.log(null == undefined);
+
+//alert( undefined > 0 ); // false (1)
+//alert( undefined < 0 ); // false (2)
+//alert( undefined == 0 ); // false (3)
+
+//alert( null > 0 ); // false
+//alert( null == 0 ); // false
+
+//alert(null >= 0); //true
+
+//alert('text');
+
+// var conf = confirm('To be or not to be?');
+// console.log(conf);
+//
+// if (conf) {
+//     var amount = prompt('How much money?');
+//     var curr = prompt('With currency?');
+//     var result = amount * curr;
+//     alert(amount + '$ with currency ' + curr + ' is equal ' + result);
+// } else {
+//     alert("Bye!");
+// }
+//
+// /
+//
+// console.log(result);
+// console.log('2' * '2');
+//
+//
+//
+// if (true) {
+//     //alert('1 < 2');
+// }
+
+// var sum = prompt('Sum');
+//
+// if (sum >= 800) {
+//     alert('Discount 20%');
+// } else if (sum < 800 && sum >= 600) {
+//     alert('Discount 10%');
+// } else if (sum < 600 && sum > 300) {
+//     alert('Discount 5%');
+// } else {
+//     alert('No discount');
+// }
+//
+// var res = (1 > 2) ? 'Yes' : 'No';
+// alert(res);
+
+//var pr = prompt('Day of the week');
+
+//}
+
+// if (pr == 'Monday' || pr == 'Tuesday' || pr == 'Thursday' ) {
+//     alert('Working day');
+// } else {
+//     alert('Not a working day');
+// }
+//
+// var x = confirm('?');
+//
+// if (!x) {
+//     alert('Yes');
+// }
+
+var a = 4;
+//
+// while (a <= 3) {
+//     alert(a);
+//     a++;
+// }
+
+
+
+//do {
+    //var age = prompt('How old are you?');
+    //alert(a);
+    //a++;
+//} while (age < 18);
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// var sum = 0;
+//
+// while (true) {
+//
+//     var value = +prompt("Введите число", '');
+//
+//     if (!value) {
+//         break;
+//     } // (*)
+//
+//     sum += value;
+//
+// }
+// alert( 'Сумма: ' + sum );
+
+for (var i = 0; i <= 10; i++) {
+
+    if(i % 2 != 0 || i == 0) {
+        continue;
+    }
+
+    console.log(i);
+}
+var month = prompt('Month?');
+switch(month) {
+    case 'January' || 'February' || 'December':  // if (x === 'value1')
+        alert('Winter');
+        break;
+
+    case '':  // if (x === 'value2')
+        break;
+
+    default:
+        alert('Wrong month.');
+        break;
+
+}
